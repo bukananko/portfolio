a04cacedaafe93f4370f5af913e1d059f9236933
Anko <totallynotankooo@gmail.com>
2023-01-31 16:26:22 +0700

    add darkmode

--- dist/js/script.js
+++ dist/js/script.js
@@ -26,3 +26,46 @@ window.addEventListener("click", function (e) {
     navMenu.classList.add("hidden");
   }
 });
+
+// darkmode
+const darkMode = document.getElementById("darkmode");
+const lightMode = document.getElementById("lightmode");
+const html = document.querySelector("html");
+
+darkMode.addEventListener("click", function () {
+  darkMode.classList.toggle("hidden");
+  lightMode.classList.toggle("hidden");
+  html.classList.add("dark");
+  localStorage.theme = "dark";
+});
+
+lightMode.addEventListener("click", function () {
+  lightMode.classList.toggle("hidden");
+  darkMode.classList.toggle("hidden");
+  html.classList.remove("dark");
+  localStorage.theme = "light";
+});
+
+// logo will move
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  darkMode.classList.add("hidden");
+  lightMode.classList.remove("hidden");
+} else {
+  // lol
+}
+
+// On page load or when changing themes, best to add inline in `head` to avoid FOUC
+if (
+  localStorage.theme === "dark" ||
+  (!("theme" in localStorage) &&
+    window.matchMedia("(prefers-color-scheme: dark)").matches)
+) {
+  document.documentElement.classList.add("dark");
+} else {
+  document.documentElement.classList.remove("dark");
+}
